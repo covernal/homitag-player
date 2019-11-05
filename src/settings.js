0f2e4fb7effa1753720ebe67c07e26d8fa809d33
covernal <covernal@hotmail.com>
2019-11-06 00:54:05 +0800

    change callback url for spotify oauth2

--- src/settings.js
+++ src/settings.js
@@ -1,3 +1,3 @@
 export const clientId = '7fb83afcfcd64f2ab96e390e5814c23a';
 export const clientSecret = '682cea19e76e4c51a9d81190a003d19c';
-export const redirectUri = 'http://localhost:3000';
+export const redirectUri = 'https://radiant-headland-04809.herokuapp.com/';
